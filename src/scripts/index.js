e740fac16fd644ae3641a54525c6237c5a9c6262
Jace Cotton <jxcotton@texaschildrens.org>
2025-03-19 14:49:55 -0500

    Fix scripts

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -6,15 +6,15 @@ import "@txch/tcds/dist/alert-bar";
 import "@txch/tcds/dist/button";
 import "@txch/tcds/dist/card";
 import "@txch/tcds/dist/carousel";
-import "@txch/tcds/dist/slide";
 import "@txch/tcds/dist/fn-ref";
 import "@txch/tcds/dist/fn-list";
-import "@txch/tcds/dist/tabs";
-import "@txch/tcds/dist/tab";
 import "@txch/tcds/dist/icon";
 import "@txch/tcds/dist/section";
-import "@txch/tcds/dist/site-header";
 import "@txch/tcds/dist/side-menu";
+import "@txch/tcds/dist/site-header";
+import "@txch/tcds/dist/slide";
+import "@txch/tcds/dist/tabs";
+import "@txch/tcds/dist/tab";
 
 import "./lib/prism.js";
 
